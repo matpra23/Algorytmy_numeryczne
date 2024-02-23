332db12174f62d481ee0b0bcf12061e902b32bef
Poczu] <m.prask1@o2.pl>
2024-02-23 22:35:07 +0100

    commit

---
+++ Zaliczenie_wykladu/main.py
@@ -0,0 +1,29 @@
+import matplotlib as plt
+import numpy as np
+
+#wizualizacja danych w układzie x, F(x,y) dla każdej linii y=const
+def func(x):
+    y = ...
+    return y
+
+def visual(x, func):
+    ...
+    plt.show()
+
+#- wyznaczyć średnią, medianę, odchylenie standardowe z podziałem na współrzędne y, prezentacja na wykresie słupkowym (obowiązkowo)
+
+#- wyznaczyć jedną funkcję interpolacyjną wielomianową dla wybranej współrzędnej y  z siatki (obowiązkowo)
+
+#- wyznaczyć funkcję interpolacyjną sklejaną dla wybranej współrzędnej y  z siatki (obowiązkowo) 
+
+#- dokonaj porównania funkcji interpolacyjnych 
+
+#- wyznaczyć dwie funkcje aproksymacyjne dla wybranej jednej współrzędnej y  z siatki - obowiązkowo jedna funkcja
+
+#- dokonaj oceny jakości aproksymacji
+
+#- obliczyć całkę z funkcji interpolacyjnych i aproksymacyjnych - obowiązkowo jedna metoda
+
+#- wyznaczyć pochodne cząstkowe dla wybranej linii punktów (y=const) (obowiązkowo)
+
+#- określić monotoniczność  dla wybranej linii punktów (y=const) (obowiązkowo)
